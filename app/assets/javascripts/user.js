c07f20966dbadeea866103eead445a7bc005c50e
om0316 <masaya.ogawa@di-v.co.jp>
2019-06-30 14:38:30 +0900

    JSONからのデータ受け取り

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,4 +1,27 @@
 $(function() {
   $(".chat-group-form__input").on("keyup", function() {
+    var input = $(".chat-group-form__input").val();
+    
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(data) {
+      if (data.length !== 0) {
+          data.forEach(function(data){
+          //appendProduct(data);
+        });
+      }
+      else {
+        //appendErrMsgToHTML("一致するユーザーはいません");
+      }
+    })
+    
+    .fail(function() {
+      alert('ユーザー検索に失敗しました');
+    })
   })
 });
