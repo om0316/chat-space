c442701d12167dfa9929d86acaf834c392694d91
om0316 <masaya.ogawa@di-v.co.jp>
2019-07-14 14:51:37 +0900

    自動デプロイ設定 修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "3.11.0"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@example.com:om0316/chat-space.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
